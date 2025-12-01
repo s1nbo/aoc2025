b0f7bf38587110078673e899dcd364d12394bad9
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-12-02 00:01:28 +0100

    01_2

--- 01.py
+++ 01.py
@@ -3,12 +3,13 @@ cur = 50
 
 for line in open("01.txt"):
     directions, steps = line[0], int(line[1:])
-    if directions == "L":
-        cur = (cur - steps) % 100
-    elif directions == "R":
-        cur = (cur + steps) % 100
-    if cur == 0:
-        ans += 1
+    for _ in range(steps):
+        if directions == "L":
+            cur = (cur - 1) % 100
+        elif directions == "R":
+            cur = (cur + 1) % 100
+        if cur == 0:
+            ans += 1
 
 print(ans)
 
