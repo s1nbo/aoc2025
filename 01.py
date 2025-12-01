230ec075ed255f8c81809f81264c7ae2b987f519
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-12-02 00:02:20 +0100

    nicer code

--- 01.py
+++ 01.py
@@ -1,16 +1,33 @@
-ans = 0
-cur = 50
+def one():
+    ans = 0
+    cur = 50
 
-for line in open("01.txt"):
-    directions, steps = line[0], int(line[1:])
-    for _ in range(steps):
+    for line in open("01.txt"):
+        directions, steps = line[0], int(line[1:])
         if directions == "L":
-            cur = (cur - 1) % 100
+            cur = (cur - steps) % 100
         elif directions == "R":
-            cur = (cur + 1) % 100
+            cur = (cur + steps) % 100
         if cur == 0:
             ans += 1
 
-print(ans)
+    print(ans)
 
-        
+
+def two():
+    ans = 0
+    cur = 50
+
+    for line in open("01.txt"):
+        directions, steps = line[0], int(line[1:])
+        for _ in range(steps):
+            if directions == "L":
+                cur = (cur - 1) % 100
+            elif directions == "R":
+                cur = (cur + 1) % 100
+            if cur == 0:
+                ans += 1
+
+    print(ans)
+
+            
